84d7290f33384e5163da0a8cc2ae049841984baa
Javier Canizalez <javier-canizalez@outlook.com>
2021-10-04 16:45:05 -0600

    Update azb.rb

--- Formula/azb.rb
+++ Formula/azb.rb
@@ -1,13 +1,13 @@
-class Azb < Formula
-    desc "azb command line tool"
-    homepage "https://github.com/AzBuilder/azb-cli"
-    url "https://github.com/azbuilder/azb-cli/releases/download/v0.2.0/azb-v0.2.0-darwin-amd64.tar.gz"
+class Terrakube < Formula
+    desc "terrakube command line tool"
+    homepage "https://terrakube.org/"
+    url "https://github.com/azbuilder/terrakube-cli/releases/download/v0.2.0/azb-v0.2.0-darwin-amd64.tar.gz"
     sha256 "c1a66489d7d2ad82ee6fefd1b80bd332d496beab6ee799dd598da91ac8da2503"
     license "Apache-2.0"
   
   
     def install
-      bin.install 'azb'
+      bin.install 'terrakube'
     end
   
   end
