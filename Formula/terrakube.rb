20317ffec3a3c720144f8be9154e6a6af9c14703
Javier Canizalez <javier-canizalez@outlook.com>
2025-04-04 17:44:21 -0700

    Updated terrakube to 1.0.0-beta

--- Formula/terrakube.rb
+++ Formula/terrakube.rb
@@ -1,8 +1,8 @@
 class Terrakube < Formula
     desc "terrakube command line tool"
     homepage "https://terrakube.org/"
-    url "https://github.com/azbuilder/terrakube-cli/releases/download/v0.3.0/terrakube-v0.3.0-darwin-amd64.tar.gz"
-    sha256 "f12095f7c1a1571f6b3372bc3c34273af5f555109373b0723458620683b8f15e"
+    url "https://github.com/azbuilder/terrakube-cli/releases/download/v1.0.0-beta/terrakube-v1.0.0-beta-darwin-amd64.tar.gz"
+    sha256 "468ea2c025dbc201c4ec7221a8bccfb6a0d4192b287e3f9d5696836ff7310f30"
     license "Apache-2.0"
   
   
