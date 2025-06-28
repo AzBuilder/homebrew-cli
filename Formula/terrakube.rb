8a59e62f4e3b0c2a17468ed6ac928894beda6394
Alfredo España <alfespa17@gmail.com>
2025-06-28 10:04:36 -0600

    Update terrakube.rb

--- Formula/terrakube.rb
+++ Formula/terrakube.rb
@@ -1,6 +1,6 @@
 class Terrakube < Formula
     desc "terrakube command line tool"
-    homepage "https://terrakube.org/"
+    homepage "https://docs.terrakube.io/"
     url "https://github.com/azbuilder/terrakube-cli/releases/download/v1.0.0-beta/terrakube-v1.0.0-beta-darwin-amd64.tar.gz"
     sha256 "468ea2c025dbc201c4ec7221a8bccfb6a0d4192b287e3f9d5696836ff7310f30"
     license "Apache-2.0"
